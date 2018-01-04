d7816a6955c30a3ac9aa9a146e0af45fff592d34
CodeYourFuture <mahsa2017>
2018-01-04 16:43:22 +0000

    added athlete error code

--- homework/advanced/challenge10.js
+++ homework/advanced/challenge10.js
@@ -1,19 +1,19 @@
-/*Challenge 8: Athlete
+/*Challenge 10: Athlete
 
 Now we are going to create an athlete object that can drink from the bottle we created 
 in the last challenge.
 
-Our athlete should hold values for their hydration level and the distance 
+Our athlete should hold *** values for their hydration level and the *** distance 
 they have travelled. 
 
-They should be able to run and drink.
+They should be able to ***run and ***drink.
 
-When they run their hydration level should decrease while their distance should 
+When they run their ***hydration level should decrease while their ***distance should 
 increase.
 
-Our athlete should not be able to run if their hydration level is less than or equal to 0.
+Our athlete should *** not be able to run if their *** hydration level is less than or equal to 0.
 
-When our athlete drinks their hydration should increase.
+When our athlete ***drinks their ***hydration should increase.
 
 --EXTRA--
 
@@ -21,8 +21,34 @@ Really our athlete should only be able to drink if the bottles volume is more th
 if you can add a conditional to your athletes drink function that only allows them
 to drink if your bottle is not empty.
 Tip: you will need to research node require
-
-
-
-
 */
+var bottle = {
+	volume: 0,
+	fill: function(){
+		return this.volume = 100;// calling this function should make you bottles volume = 100; 
+	},
+	drink: function(){
+		return this.volume -= 10 ;// calling this function should decrease your bottles volume by 10;
+	},
+	empty: function(){
+		if(!this.volume)
+		return true;// this function should return true if your bottles volumn = 0
+	}
+}
+var athlete={
+    hydration:0,
+    distance:0,
+
+    run: function(){
+        if(this.hydration > 0){
+        this.hydration--;
+        this.distance++;
+        }
+    },
+    drink: function(){
+        if(!bottle.empty())
+        return hydration++;
+    },
+}
+athlete.run();
+athlete.drink();
